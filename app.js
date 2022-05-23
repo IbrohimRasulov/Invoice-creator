6d1295d7f3206fe199f15a4528bb861b6d9f51da
Ibrohim <ibrohim05062000@gmail.com>
2022-05-24 03:47:46 +0500

    Complete functionality of app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const servicesBtn = document.querySelectorAll('#services button');
 const tasksList = document.querySelector('#tasks ul');
 const totalCost = document.querySelector('#total-cost');
+const notes = document.querySelector('#notes');
 
 const myServices = [];
 
@@ -14,51 +15,84 @@ servicesBtn.forEach(service => {
       myServices.push(e.target.value)
     }
 
-    tasksList.innerHTML = `
-    <li>
-      <h4>TASK</h4>
-      <h4>TOTAL</h4>
-    </li>
-    `;
-
-    myServices.forEach(singleService => {
-      if (singleService === 'Wash Car') {
-        tasksList.innerHTML += `
-        <li>
-          <p>${singleService}</p>
-          <img src="assets/icon-delete.svg" alt="delete-btn">
-          <p><span>$</span>10</p>
-        </li>
-        `;
-      } else if (singleService === 'Mow Lawn') {
-        tasksList.innerHTML += `
-        <li>
-          <p>${singleService}</p>
-          <img src="assets/icon-delete.svg" alt="delete-btn">
-          <p><span>$</span>20</p>
-        </li>
-        `;
-      } else if (singleService === 'Pull Weeds') {
-        tasksList.innerHTML += `
-        <li>
-          <p>${singleService}</p>
-          <img src="assets/icon-delete.svg" alt="delete-btn">
-          <p><span>$</span>30</p>
-        </li>
-        `;
-      }
-    });
-
-    const cost = myServices.map(item => {
-      if (item === 'Wash Car') {
-        return 10;
-      } else if (item === 'Mow Lawn') {
-        return 20;
-      } else if (item === 'Pull Weeds') {
-        return 30;
-      }
-    });
-
-    totalCost.textContent = cost.reduce((a,b) => a + b, 0);
+    clearList();
+    displayList();
+    total();
   });
 });
+
+const clearList = function () {
+  tasksList.innerHTML = `
+  <li>
+    <h4>TASK</h4>
+    <h4>TOTAL</h4>
+  </li>
+  `;
+}
+
+const total = function () {
+  const cost = myServices.map(item => {
+    if (item === 'Wash Car') {
+      return 10;
+    } else if (item === 'Mow Lawn') {
+      return 20;
+    } else if (item === 'Pull Weeds') {
+      return 30;
+    }
+  });
+
+  totalCost.textContent = cost.reduce((a, b) => a + b, 0);
+
+  if (myServices.length !== 0) {
+    notes.textContent = 'We accept cash, credit card, or PayPal';
+  } else if (myServices.length === 0) {
+    notes.textContent = '';
+  }
+}
+
+const displayList = function () {
+  myServices.forEach(singleService => {
+    if (singleService === 'Wash Car') {
+      tasksList.innerHTML += `
+      <li>
+        <p>${singleService}</p>
+        <img src="assets/icon-delete.svg" alt="delete-btn">
+        <p><span>$</span>10</p>
+      </li>
+      `;
+    } else if (singleService === 'Mow Lawn') {
+      tasksList.innerHTML += `
+      <li>
+        <p>${singleService}</p>
+        <img src="assets/icon-delete.svg" alt="delete-btn">
+        <p><span>$</span>20</p>
+      </li>
+      `;
+    } else if (singleService === 'Pull Weeds') {
+      tasksList.innerHTML += `
+      <li>
+        <p>${singleService}</p>
+        <img src="assets/icon-delete.svg" alt="delete-btn">
+        <p><span>$</span>30</p>
+      </li>
+      `;
+    }
+
+    addEventToDeleteBtn();
+  });
+}
+
+const addEventToDeleteBtn = function () {
+  document.querySelectorAll('img[alt="delete-btn"]').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      const clickedBtn = e.target.parentNode.firstElementChild.textContent;
+
+      const index = myServices.indexOf(clickedBtn);
+      myServices.splice(index, 1);
+
+      clearList();
+      displayList();
+      total();
+    });
+  });
+}
